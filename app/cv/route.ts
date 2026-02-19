0afef06c8cb5541b08ed426288b2d00e5eef9e53
nocdn <bartek.bak@protonmail.com>
2026-02-19 17:16:52 +0000

    feat: add cv route which serves my pdf file

---
+++ app/cv/route.ts
@@ -0,0 +1,15 @@
+import { readFile } from "fs/promises"
+import { join } from "path"
+
+export async function GET() {
+  const filePath = join(process.cwd(), "public", "Bartosz_Bak_CV.pdf")
+  const fileBuffer = await readFile(filePath)
+
+  return new Response(fileBuffer, {
+    headers: {
+      "Content-Type": "application/pdf",
+      "Content-Disposition": 'inline; filename="Bartosz_Bak_CV.pdf"',
+      "Content-Length": fileBuffer.byteLength.toString(),
+    },
+  })
+}
